c2ced522c05018e1c799ca2571f9944ed315d96f
daniberv <zarkonvvv@gmail.com>
2024-12-28 23:51:04 +0300

    ts skip mode config

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,37 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import fs from 'fs'
 
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-    '@': `${__dirname}/src`,
-    },
-  },
-  server: {
-    port: 443,
-    host: "0.0.0.0",
-    hmr: {
-        host: 'tg-mini-app.local',
+export default ({ mode }) => {
+  console.log('MODE HERE', mode)
+  if (mode === "development") {
+    return defineConfig({
+      plugins: [react()],
+      resolve: {
+        alias: {
+        '@': `${__dirname}/src`,
+        },
+      },
+      server: {
         port: 443,
-    },
-    https: {
-      key: fs.readFileSync('./.cert/localhost-key.pem'),
-      cert: fs.readFileSync('./.cert/localhost.pem'),
-    },
-  },
-})
+        host: "0.0.0.0",
+        hmr: {
+            host: 'tg-mini-app.local',
+            port: 443,
+        },
+        https: {
+          key: fs.readFileSync('./.cert/localhost-key.pem'),
+          cert: fs.readFileSync('./.cert/localhost.pem'),
+        },
+      },
+    })
+  } else {
+    return defineConfig({
+      plugins: [react()],
+      resolve: {
+        alias: {
+        '@': `${__dirname}/src`,
+        },
+      }
+    })
+  }
+}
